f80718a0f85be1ebabcb56549fd548c3be7de6e9
nurulcse7 <nurul.cse7@gmail.com>
2022-10-19 22:16:12 +0600

    first commit

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='text-center mt-16 sm:fixed-bottom'>
+            <p><small>Copyright coming soon</small></p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFXlVf0Zt2KL7R7HaRFJW6kRZfVjVQEe0",
+  authDomain: "dragon-news-4e9ab.firebaseapp.com",
+  projectId: "dragon-news-4e9ab",
+  storageBucket: "dragon-news-4e9ab.appspot.com",
+  messagingSenderId: "440428856647",
+  appId: "1:440428856647:web:6792ca2dc3cd1796723862"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
