5c4fa2621978d4278d5698e5e652000a5841e73e
nurulcse7 <nurul.cse7@gmail.com>
2022-10-20 10:29:26 +0600

    title updated

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Category = () => {
-    return (
-        <div>
-            <h2>This is Category</h2>
-        </div>
-    );
+  const categoryNews = useLoaderData();
+
+  return (
+    <div>
+      <h2>This is Category has news: {categoryNews.length}</h2>
+    </div>
+  );
 };
 
 export default Category;
