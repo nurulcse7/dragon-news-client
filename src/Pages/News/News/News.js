888ea51de555ed080f2f6b8b0e243e32597b7d84
nurulcse7 <nurul.cse7@gmail.com>
2022-10-29 10:17:45 +0600

    useTitle added

--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import useTitle from '../../../hooks/useTitle';
 
 const News = () => {
     const news = useLoaderData();
     console.log(news);
+    useTitle('News Details');
     const { title, details, image_url, category_id } = news;
     return (
         <Card>
